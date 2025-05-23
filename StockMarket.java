88f677ca0ba2c0bf451bc5fe1763a40b6a4c38b4
novacream <ubuntu@ip-172-31-0-19.us-east-2.compute.internal>
2025-05-23 04:56:03 +0000

    data > dataDir to deconflict name with future .dat file

--- StockMarket.java
+++ StockMarket.java
@@ -3,9 +3,9 @@ import java.io.*;
 public class StockMarket{
 	void main(){
 		System.out.println("Welcome!\n");
-		File data=new File("data\\");
-		if(!data.exists()){
-			data.mkdir();
+		File dataDir=new File("data\\");
+		if(!dataDir.exists()){
+			dataDir.mkdir();
 		}
 		
 	}
@@ -14,6 +14,6 @@ public class StockMarket{
 		//possibly supply/demand info? 
 			//more buys means higher prices/supply, fewer buys means lower prices/supply, doesn't apply to some limited stocks
 		//all user inventories
-		
+	//output should be in plaintext
 }
 
